b599f396a8f1e58f534bc9a7120e73decb973a05
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-07-29 16:59:36 +0300

    create gateway server

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"time"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":0")
+	port := flag.Int("port", 8000, "Gateway port")
+	flag.Parse()
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello darkness my old friend \n"))
+		w.Write([]byte("hello darkness my old friend from the gateway\n"))
 	})
 
 	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", *port),
 		Handler:      handler,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Starting server on %s", listener.Addr())
+	log.Printf("Starting server on %s", srv.Addr)
 
-	err = srv.Serve(listener)
+	err := srv.ListenAndServe()
 	log.Fatal(err)
 }
