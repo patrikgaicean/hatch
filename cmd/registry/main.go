09f155af1bcb179ab0b64956e0889815c66dbcf9
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-08-03 00:39:14 +0300

    update service and registry

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("the service registry")
+	port := flag.Int("port", 8080, "Registry port")
+	flag.Parse()
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("registry says - %s hit me\n", r.RemoteAddr)
+	})
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", *port),
+		Handler:      handler,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Printf("Starting server on %s", srv.Addr)
+
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"time"
 )
 
+func register() {
+	res, err := http.Get("http://localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("status: %d - body: %s\n", res.StatusCode, body)
+}
+
+func unregister() {}
+func heartbeat()  {}
+
 func main() {
+	// perform heartbeat
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+			heartbeat()
+		}
+	}()
+
 	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// register to service registry
+	register()
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello darkness my old friend \n"))
@@ -22,7 +55,11 @@ func main() {
 	}
 
 	log.Printf("Starting server on %s", listener.Addr())
-
 	err = srv.Serve(listener)
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// on server <graceful> shutdown, de-register from service registry
+	// TODO
 }
