62d5b023eaf942736849105d1ae98edea7d41034
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-08-04 20:52:29 +0300

    messing around with structure defs and marshaling

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
+
+type ServiceInfo struct {
+	Ip    string
+	Dunno string
+}
 
 func main() {
 	port := flag.Int("port", 8080, "Registry port")
 	flag.Parse()
 
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	ctx := context.Background()
+	// err := client.Set(ctx, "foo", "bar", 0).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	val, err := client.Get(ctx, "foo").Result()
+	if err != nil {
+		switch {
+		case errors.Is(err, redis.Nil):
+			fmt.Println("no value found")
+		default:
+			panic(err)
+		}
+	}
+	fmt.Println("foo = ", val)
+
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("registry says - %s hit me\n", r.RemoteAddr)
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.Body.Close()
+
+		var dat2 ServiceInfo
+		if err := json.Unmarshal([]byte(body), &dat2); err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v", dat2)
 	})
 
 	srv := &http.Server{
@@ -26,6 +70,6 @@ func main() {
 
 	log.Printf("Starting server on %s", srv.Addr)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	log.Fatal(err)
 }
--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net"
 	"net/http"
 	"time"
 )
 
-func register() {
-	res, err := http.Get("http://localhost:8080")
+type ServiceInfo2 struct {
+	ip    string
+	dunno string
+	Other string
+	Prop  string
+}
+
+type ServiceInfo struct {
+	Ip    string
+	Dunno string
+}
+
+func register(addr string) {
+	payload := &ServiceInfo{
+		Ip:    addr,
+		Dunno: "hi there",
+	}
+	jsonData, _ := json.Marshal(payload)
+	fmt.Println(string(jsonData))
+
+	res, err := http.Post(
+		"http://localhost:8080",
+		"application/json",
+		bytes.NewBuffer(jsonData),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,6 +52,20 @@ func unregister() {}
 func heartbeat()  {}
 
 func main() {
+	p := ServiceInfo2{
+		ip:    "123",
+		dunno: "456",
+	}
+
+	fmt.Printf("p = %+v\n", p)
+	r := ServiceInfo2{
+		ip:    "12344",
+		dunno: "45664",
+		Other: "12323",
+		Prop:  "ksmggs",
+	}
+	fmt.Printf("r = %+v\n", r)
+
 	// perform heartbeat
 	go func() {
 		for {
@@ -41,7 +80,7 @@ func main() {
 	}
 
 	// register to service registry
-	register()
+	// register(fmt.Sprint(listener.Addr()))
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello darkness my old friend \n"))
