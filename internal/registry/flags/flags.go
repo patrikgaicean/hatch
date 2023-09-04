2a853fce70eed1835da3fb248d442994f7722d1d
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-09-05 00:23:19 +0300

    move default flags to the flags package

--- internal/registry/flags/flags.go
+++ internal/registry/flags/flags.go
@@ -20,6 +20,18 @@ type redis struct {
 	Password string
 }
 
+var defaults flags = flags{
+	IP:      "127.0.0.1",
+	Port:    8080,
+	Env:     "development",
+	Cleanup: 15,
+	Redis: redis{
+		Host:     "127.0.0.1",
+		Port:     6973,
+		Password: "password123",
+	},
+}
+
 func ParseFlags() flags {
 	var (
 		f          flags
